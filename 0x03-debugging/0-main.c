0251a34d69ee9dfc60d3e880eb22f3ae738e03a4
sheilaowiso <sheilajoylasoi@gmail.com>
2022-04-13 01:44:23 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *main - tests function prints if integer positive or negative
  *
@@ -9,6 +8,9 @@ int main(void)
 {
 int i;
 i = 0;
+
 positive_or_negative(i);
+
 return (0);
+
 }
