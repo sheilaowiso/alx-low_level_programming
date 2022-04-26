82b9b044e27611d4767e8d69f11e165e3502f9d0
sheilaowiso <sheilajoylasoi@gmail.com>
2022-04-26 04:32:45 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -9,11 +8,9 @@
  */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; s[i] != '\0' ; i++)
-{
+unsigned int i;
+for (i = 0; s[i] != '\0'; i++)
 if (s[i] == c)
-return (&s[i]);
-}
-return (0);
+break;
+return (s[i] == c ? (s + i) : '\0');
 }
