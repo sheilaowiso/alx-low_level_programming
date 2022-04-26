1998ef429f6c9d5dd93e689e8fd371e857404c29
sheilaowiso <sheilajoylasoi@gmail.com>
2022-04-26 04:28:27 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,6 @@
  * _strchr - locates a character in a string
  * @s: the string
  * @c: the character
- *
  * Return: a pointer to a character aka the character found
  */
 char *_strchr(char *s, char c)
